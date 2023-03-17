ac5d972f34dba3dc39cc4c950a9003630ef5000d
kofoworola <kofoworolao11@gmail.com>
2023-03-17 10:56:17 -0700

    help

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,24 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- *main -Entry point
- *Description: 'get the last digit of a number'
- *Return: always 0
+ * main - Entry point
+ * Description: prints output
+ * Return: 0
  */
 int main(void)
 {
-	int n;
-	int m;
+int n, m;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	m  = n % 10;
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	else if (m == 0)
-		printf("last digit of %d is %d amd is 0\n", n, m);
-	else if (m < 6 && m != 0)
-		printf("last digit of %d is %d and less than 6 and not 0\n", n, m);
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+m = n % 10;
+
+	/* your code goes there */
+if (m < 6 && m != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+}
+else if (m > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, m);
+}
+else if (m == 0)
+{
+printf("Last digit of %d is 0 and is %d\n", n, m);
+}
+return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,13 @@
 
 int main(void)
 {
-	int n = 97;
-	int m = 65;
 
-	while (n <= 90)
-	{
-		putchar(n);
-		n++;
-	}
-	while (m <= 90)
-	{
-		putchar(m);
-		m++;
-	}
-	putchar('\n');
+	char i;
+
+	for (i = 'a'; i <= 'Z; i++);
+		putchar(i);
+	for (i= 'A; i <= 'Z'; i++);
+		putchar(i);
+	putchar ('\n');
 	return (0);
 }
