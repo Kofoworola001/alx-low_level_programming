164ba9a6c36c2f04b89b6ca94958517564b887cb
kofoworola <kofoworolao11@gmail.com>
2023-03-21 14:09:56 -0700

    putchar

--- 0x02-functions_nested_loops/9-times_tables.c
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -1,40 +1,48 @@
-[13:32, 21/03/2023] Emma ALX: #include "main.h"
-
+#include "main.h"
 /**
- * times_table - prints the 9 times table, starting with 0.
+ * times_table -prints tables
  *
- * Return: Times table
+ * Return: Always 0.
  */
-
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int a;
+	int b;
+	int c;
 
-	for (x = 0; x <= 9; x++)
+	for (a = 0; a <= 9; a++)
 	{
-	for (y = 0; y <= 9; y++)
-	{
-	z = x * y;
-	if (z > 9)
-	{
-	u = z % 10;
-	d = (z - u) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(d + '0');
-	_putchar(u + '0');
-	}
-	else
-	{
-	if (y != 0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(z + '0');
-	}
-	}
-	_putchar('\n');
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if ((c / 10) == 0)
+			{
+				if (b == 0)
+				{
+					_putchar ('0');
+				}
+				if (b != 0)
+				{
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar ('\n');
 	}
 }
