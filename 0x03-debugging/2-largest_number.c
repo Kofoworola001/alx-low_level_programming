d56f81bed23f223a0c7f6bb4fd403d49d64778ae
kofoworola <kofoworolao11@gmail.com>
2023-03-23 01:32:50 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,25 +2,66 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first int
- * @b: second int
- * @c: third int
+ * @num1: first integer
+ * @num2: second integer
+ * @num3: third integer
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
-{
-	int largest = b;
+int largest_number(int num1, int num2, int num3);
 
-	if (a > b)
+/**
+ * largest_number - return max
+ * @num1: the first number
+ * @num2: the second number
+ * @num3: the third number
+ * Return: the largest number
+ */
+
+int largest_number(int num1, int num2, int num3)
+{
+	int largest;
+
+	if (num1 > num2 && num1 > num3)
 	{
-		largest = a;
+		largest = num1;
 	}
-	else if (c > b)
+	else if (num2 > num1 && num2 > num3)
 	{
-		largest = c;
+		largest = num2;
+	}
+	else if (num3 > num1 && num3 > num2)
+	{
+		largest = num3;
+	}
+	else if (num1 == num2 && num1 > num3)
+	{
+		largest = num1;
+	}
+	else if (num1 == num3 && num1 > num2)
+	{
+		largest = num1;
+	}
+	else if (num2 == num3 && num2 > num1)
+	{
+		largest = num2;
+	}
+	else if (num2 == num1 && num2 > num3)
+	{
+		largest = num2;
+	}
+	else if (num3 == num1 && num3 > num2)
+	{
+		largest = num3;
+	}
+	else if (num3 == num2 && num3 > num1)
+	{
+		largest = num3;
+	}
+	else
+	{
+		largest = num1 = num2 = num3;
 	}
 
 	return (largest);
 }
-
