36585ce9b210d1fc9500d38b502adc1a6ec77471
kofoworola <kofoworolao11@gmail.com>
2023-03-23 01:35:21 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,67 +1,23 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @num1: first integer
- * @num2: second integer
- * @num3: third integer
- * Return: largest number
+ * main - prints the largest of 3 integers
+ * Return: 0
  */
 
-int largest_number(int num1, int num2, int num3);
-
-/**
- * largest_number - return max
- * @num1: the first number
- * @num2: the second number
- * @num3: the third number
- * Return: the largest number
- */
-
-int largest_number(int num1, int num2, int num3)
+int main(void)
 {
+	int a, b, c;
 	int largest;
 
-	if (num1 > num2 && num1 > num3)
-	{
-		largest = num1;
-	}
-	else if (num2 > num1 && num2 > num3)
-	{
-		largest = num2;
-	}
-	else if (num3 > num1 && num3 > num2)
-	{
-		largest = num3;
-	}
-	else if (num1 == num2 && num1 > num3)
-	{
-		largest = num1;
-	}
-	else if (num1 == num3 && num1 > num2)
-	{
-		largest = num1;
-	}
-	else if (num2 == num3 && num2 > num1)
-	{
-		largest = num2;
-	}
-	else if (num2 == num1 && num2 > num3)
-	{
-		largest = num2;
-	}
-	else if (num3 == num1 && num3 > num2)
-	{
-		largest = num3;
-	}
-	else if (num3 == num2 && num3 > num1)
-	{
-		largest = num3;
-	}
-	else
-	{
-		largest = num1 = num2 = num3;
-	}
+	a = 972;
+	b = -98;
+	c = 0;
 
-	return (largest);
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
 }
