3d69bb50d6b7acd4bf9fef804dc9d86f23f0beca
kofoworola <kofoworolao11@gmail.com>
2023-04-17 13:28:44 -0700

    my commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,11 @@
 #include "dog.h"
 
 /**
- * init_dog - intializes a variable of type struct dog.
+ * init_dog - initializes a variable of type struct dog.
  * @d: struct dog.
  * @name: name of the dog.
  * @age: age of the dog.
- * @owner of the dog.
+ * @owner: owner of the dog.
  *
  * Return: no return.
  */
