b32c4a6fe5320f6b7afbc90bae3aedb36c0ecab2
kofoworola <kofoworolao11@gmail.com>
2023-05-04 13:07:58 -0700

    index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,9 +1,9 @@
-nclude "main.h"
+#include "main.h"
 /**
- * set_bit - sets the value of a bit to 1 at a given index
- * @n: unsigned long to change
- * @index: index to change to one
- * Return: 1 if it worked or -1 if an error occured
+  * set_bit - sets the value of a bit to 1 at a given index
+  * @n: unsigned long to change
+  * @index: index to change to one
+  * Return: 1 if it worked or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
