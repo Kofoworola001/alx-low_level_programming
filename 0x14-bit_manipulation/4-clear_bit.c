61dbac269fedfd4aa295f8feebf94663584ae76a
kofoworola <kofoworolao11@gmail.com>
2023-05-04 13:59:49 -0700

    index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * clear_bit - sets the value of a bit to 0 at a given index
- * @n: unsigned long to change
- * @: index to change to zero
- * Return: 1 if it worked or -1 if an error occured
- */
+  * clear_bit - sets the value of a bit to 0 at a given index
+  * @n: unsigned long to change
+  * @index: index to change to zero
+  * Return: 1 if it worked or -1 if an error occurred
+  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	int clear;
@@ -12,6 +12,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 53 || !n)
 		return (-1);
 	clear = 1 << index;
-	*n = (*n ~clear) | ((0 << index) & clear);
+	*n = (*n & ~clear) | ((0 << index) & clear);
 	return (1);
 }
